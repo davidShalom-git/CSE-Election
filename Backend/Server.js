a7e804da4a8f9d8784bd191a450cea15f40451c1
davidshalom-git <davidshalom2609@gmail.com>
2025-08-10 13:19:23 +0530

    CSE

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,33 +1,28 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const user = require('./router/User')
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const user = require('./router/User');
 
+const app = express();
 
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
 
-app.use(cors())
-app.use(bodyParser.json())
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('DB connected');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
 
+// Routes
+app.use('/api/vote', user);
 
-mongoose.connect('mongodb://127.0.0.1:27017/vote').then((res)=>{
-    console.log('db connected')
-}).catch((err)=> {
-    console.log(err)
-})
-
-
-app.use('/api/vote',user)
-
-
-
-
-app.listen(1200,(req,res)=> {
-    console.log('server is running on port 1200')
-})
-
-
-
-
+// Export the handler for Vercel
+module.exports = app;
