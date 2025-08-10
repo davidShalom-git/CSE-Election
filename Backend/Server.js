bd6740aab4ac73181ebc0ff34c4ad395afd33d5d
davidshalom-git <davidshalom2609@gmail.com>
2025-08-10 13:30:14 +0530

    CSE

--- Backend/Server.js
+++ Backend/Server.js
@@ -24,5 +24,10 @@ mongoose
 // Routes
 app.use('/api/vote', user);
 
+// Add a root route
+app.get('/', (req, res) => {
+  res.json({ message: 'Voting API is running!' });
+});
+
 // Export the handler for Vercel
 module.exports = app;
